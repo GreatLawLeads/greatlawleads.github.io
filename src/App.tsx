b153d39954ff2fc0f6b4d7cf52f4cd044a77a16a
Carlos Garcia <carlos@carlosgarciapro.com>
2025-02-13 10:20:19 -0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+// import viteLogo from '/logo.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://greatlawleads.com" target="_blank">
+          {/* <img src={viteLogo} className="logo" alt="Great Law Leads logo" /> */}
+          Great Law Leads
+        </a>
+      </div>
+      <h1>Coming soon</h1>
+      <div className="card">
+        <p>
+          
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Reach us at info@greatlawleads.com
+      </p>
+    </>
+  )
+}
+
+export default App
